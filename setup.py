5c259b6f967970a7b29678a031b8248a1f795367
Jianbo <zhjb1996@163.com>
2024-03-05 15:34:34 +0800

    merge setup.cfg metainfo to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='kappamin',
+    version='0.2.0',
+    url='https://github.com/JianboHIT/kappamin',
+    author='Jianbo ZHU',
+    description='A python3 code for calculations of the minimum limit to thermal conductivity',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords=['thermal-conductivity', 'limitation', 'condensed-matter-physics', 'phonon-transport'],
+    license='Apache-2.0 license',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+    py_modules=['kappamin'],
+    install_requires=[
+        'numpy',
+        'scipy'
+    ],
+)
